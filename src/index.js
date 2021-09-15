3b1dc893d4eaee3a8430061f8be86f3b2dbd0d3b
chucoyos <facturasmanzanillo@hotmail.com>
2021-09-14 21:39:16 -0500

    add component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import CommentDetail from './CommentDetail'
 
 const App = () => {
     return (
         <div className="ui container comments">
-          <CommentDetail 
-            author={faker.name.firstName()} 
-            timeAgo={faker.date.weekday()} 
-            phrase={faker.hacker.phrase()} 
-            avatar={faker.image.avatar()} />
-          <CommentDetail 
-            author={faker.name.firstName()} 
-            timeAgo={faker.date.weekday()} 
-            phrase={faker.hacker.phrase()} 
-            avatar ={faker.image.avatar()} />
-          <CommentDetail 
-            author={faker.name.firstName()} 
-            timeAgo={faker.date.weekday()} 
-            phrase={faker.hacker.phrase()} 
-            avatar={faker.image.avatar()} />
+          <div className="ui divider"></div>
+          <ApprovalCard>
+            Are you Shure?
+          </ApprovalCard>
+          <ApprovalCard>
+            <CommentDetail 
+              author={faker.name.firstName()} 
+              timeAgo={faker.date.weekday()} 
+              phrase={faker.hacker.phrase()} 
+              avatar={faker.image.avatar()} 
+            />
+          </ApprovalCard>
+
+          <ApprovalCard>
+            <CommentDetail 
+              author={faker.name.firstName()} 
+              timeAgo={faker.date.weekday()} 
+              phrase={faker.hacker.phrase()} 
+              avatar={faker.image.avatar()} 
+            />
+          </ApprovalCard>
         </div>
     );
 }
